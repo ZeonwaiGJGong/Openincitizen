1d05f29d9d85a91b6f07a8d5349756f1b2b8d2c6
Zw.GJ.Gong <50037000+ZeonwaiGJGong@users.noreply.github.com>
2019-07-20 00:32:17 +0800

    Update NetFeatures.py

--- NetFeatures.py
+++ NetFeatures.py
@@ -1,10 +1,12 @@
 def train(self):
 #卷积函数
-    feature1=[]
+    feature1=[] 
+    #feature1并没有使用于本次运行
     for i in range(int(len(self)/2)):
         a=self[i*2]
         feature1.append(a)
     feature2=[]
+    #每隔两位抽出一位数值为特征
     for i in range(0,int(len(self)/3)):
         a=self[i*3]
         feature2.append(a)
@@ -12,12 +14,13 @@ def train(self):
     return feature2
 
 x_data=[]
-#非原生数据包
+#用于非原始数据因为在运行中会产生易位调整
 for i in range(10):
     a=input()
     x_data.append(a)
 
 result=[]
+#偶数条件下的作为结果的输出变量
 tg=train(x_data)
 for i in range(int(len(tg))):
     if int(tg[i])%2!=0:
@@ -38,4 +41,5 @@ for i in range(int(len(tg))):
 print(x_data)
 print('目标结果\n',result)
 #找出偶数
-
+#后言:
+#作为认识卷积概念的一次代码,没有打算展开只是上传记录一下,也有待进一步整理.
